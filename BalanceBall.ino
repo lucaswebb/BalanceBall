b05d22b7e4bc8cf5e85ec986e0fb4b2b6547286f
Sam Noyes <Clam99@users.noreply.github.com>
2016-02-26 17:15:52 -0500

    Update BalanceBall.ino

--- BalanceBall.ino
+++ BalanceBall.ino
@@ -1,8 +1,10 @@
 #include <Servo.h>
 
-String val; //recieved value
 Servo xServo;
 Servo yServo;
+char inData[1000]; // Allocate some space for the string
+byte index = 0; // Index into array; where to store the character
+char inChar=-1;
 
 void setup() {
   // put your setup code here, to run once:
@@ -14,25 +16,27 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  if (Serial.available()>0) 
-   { // If data is available to read,
-      val = Serial.read(); // read it and store it in val
-     
+  while(Serial.available() > 0) // Don't read unless
+   // there you know there is data
+ {
+   if(index < 19) // One less than the size of the array
+   {
+     inChar = Serial.read(); // Read a character
+     inData[index] = inChar; // Store it
+     index++; // Increment where to write next
+     inData[index] = '\0'; // Null terminate the string
    }
-   if (val != "") {
+ }
+ index = 0;
+ String val(inData);
     String myString = val;
     int commaIndex = myString.indexOf(',');
     String firstValue = myString.substring(0, commaIndex);
     String secondValue = myString.substring(commaIndex+1);
-    float bluestX = firstValue.toFloat();
-    float bluestY = secondValue.toFloat();
-    xServo.write(bluestX*180);
-    yServo.write(bluestY*180);
+    int bluestX = firstValue.toInt();
+    int bluestY = secondValue.toInt();
+    xServo.write(bluestX);
+    yServo.write(bluestY);
     digitalWrite(13,HIGH);
-   }
-   else {
-    digitalWrite(13,LOW);
-   }
-    Serial.println("Val is " + val);
    delay(50);
 }
